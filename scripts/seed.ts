f34c93c36d542b9c8305f736e733c53c4a5a94e9
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2025-08-15 14:49:20 +0330

    Setup faker to seed database with sample data

---
+++ scripts/seed.ts
@@ -0,0 +1,66 @@
+import "dotenv/config";
+import { db } from "@/db/client";
+import { products, admins, productCategories } from "@/db/schema";
+import { sql } from "drizzle-orm";
+import bcrypt from "bcrypt";
+import { faker } from "@faker-js/faker";
+
+type InsertProduct = typeof products.$inferInsert;
+
+// Function to generate a random number of products
+function generateProducts(count: number): InsertProduct[] {
+  const generatedProducts: InsertProduct[] = [];
+  for (let i = 0; i < count; i++) {
+    const category = faker.helpers.arrayElement(productCategories);
+
+    generatedProducts.push({
+      name: faker.commerce.productName(),
+      supply: faker.number.int({ min: 10, max: 200 }),
+      price: faker.number.int({ min: 100, max: 50000 }), // price in cents
+      category: category,
+    });
+  }
+  return generatedProducts;
+}
+
+async function seed() {
+  console.log("Seeding database...");
+
+  try {
+    console.log("Clearing existing data...");
+    await db.run(sql`
+      DROP TABLE IF EXISTS "order_items";
+      DROP TABLE IF EXISTS "product_history";
+      DROP TABLE IF EXISTS "products";
+      DROP TABLE IF EXISTS "orders";
+      DROP TABLE IF EXISTS "admins";
+    `);
+
+    console.log("Please re-run your migrations to recreate the tables.");
+
+    console.log("Inserting initial data...");
+
+    // Seed the admin user
+    const adminPassword = "password123";
+    const hashedPassword = await bcrypt.hash(adminPassword, 10);
+
+    await db.insert(admins).values({
+      email: "admin@storetrack.com",
+      hashedPassword: hashedPassword,
+    });
+
+    // Seed products
+    const sampleProducts = generateProducts(100);
+    await db.insert(products).values(sampleProducts);
+    console.log("50 products seeded successfully.");
+
+    console.log("Database seeded successfully!");
+  } catch (error) {
+    console.error("Database seeding failed:", error);
+    process.exit(1);
+  } finally {
+    process.exit(0);
+  }
+}
+
+seed();
